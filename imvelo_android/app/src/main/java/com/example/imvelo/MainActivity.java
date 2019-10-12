d031f7c8d1495bf48ce082d8f0f8a3abfaf8f8e1
Paul595 <45532233+paul595@users.noreply.github.com>
2019-10-12 23:16:55 +0200

    TitleScreen fertig bis auf Schriftart

--- imvelo_android/app/src/main/java/com/example/imvelo/MainActivity.java
+++ imvelo_android/app/src/main/java/com/example/imvelo/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.imvelo;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Window;
 import android.widget.SeekBar;
 
 import com.john.waveview.WaveView;
@@ -12,12 +13,8 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getSupportActionBar().hide(); //<< this
         setContentView(R.layout.activity_main);
-        //create Wave
-        final WaveView wave = (WaveView)findViewById(R.id.waveView);
-        wave.setProgress(45);
-        final WaveView wave2 = (WaveView)findViewById(R.id.waveView2);
-        wave2.setProgress(35);
-        final WaveView wave3 = (WaveView)findViewById(R.id.waveView3);
     }
+
 }
