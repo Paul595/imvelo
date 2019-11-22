b650343f044b96ae818d1702484f21b3e971024a
Paul595 <45532233+Paul595@users.noreply.github.com>
2019-11-22 19:22:12 +0100

    BottomBar

--- imvelo_flutter/imvelo_flutter/lib/main.dart
+++ imvelo_flutter/imvelo_flutter/lib/main.dart
@@ -9,18 +9,11 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'ImveloHome',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.green,
       ),
-      home: MyHomePage(title: 'Imvelo'),
+      home: MyStatefulWidget(), 
+      
+        
     );
   }
 }
@@ -145,6 +138,70 @@ class _MyHomePageState extends State<MyHomePage> {
     
     
 
+    );
+  }
+}
+
+class MyStatefulWidget extends StatefulWidget {
+  MyStatefulWidget({Key key}) : super(key: key);
+
+  @override
+  _MyStatefulWidgetState createState() => _MyStatefulWidgetState();
+}
+
+class _MyStatefulWidgetState extends State<MyStatefulWidget> {
+  int _selectedIndex = 0;
+  static const TextStyle optionStyle =
+      TextStyle(fontSize: 30, fontWeight: FontWeight.bold);
+  static const List<Widget> _widgetOptions = <Widget>[
+    Text(
+      'Index 0: TIPS',
+      style: optionStyle,
+    ),
+    Text(
+      'Index 1: Barcode Scanner',
+      style: optionStyle,
+    ),
+    Text(
+      'Index 3: Einstellungen',
+      style: optionStyle,
+    ),
+  ];
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Imvelo'),
+      ),
+      body: Center(
+        child: _widgetOptions.elementAt(_selectedIndex),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(Icons.lightbulb_outline),
+            title: Text('Tipps'),
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.camera_alt),
+            title: Text('Scanner'),
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.settings),
+            title: Text('Einstellungen'),
+          ),
+        ],
+        currentIndex: _selectedIndex,
+        selectedItemColor: Colors.amber[800],
+        onTap: _onItemTapped,
+      ),
     );
   }
 }
